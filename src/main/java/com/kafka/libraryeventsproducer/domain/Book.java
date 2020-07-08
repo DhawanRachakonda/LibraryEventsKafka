a195a8e343b203b8c116121ffefc91417b459bad
H352615 <dhawan.rachakonda@honeywell.com>
2020-07-08 19:01:10 +0530

    feat: h2 jpa

--- src/main/java/com/kafka/libraryeventsproducer/domain/Book.java
+++ src/main/java/com/kafka/libraryeventsproducer/domain/Book.java
@@ -5,14 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@Entity
 public class Book {
 
+    @Id
     private Integer bookId;
     private String bookName;
     private String bookAuthor;
+    @OneToOne
+    @JoinColumn(name = "libraryEventId")
+    private LibraryEvent libraryEvent;
 
 }
--- src/main/java/com/kafka/libraryeventsproducer/domain/LibraryEvent.java
+++ src/main/java/com/kafka/libraryeventsproducer/domain/LibraryEvent.java
@@ -1,17 +1,22 @@
 package com.kafka.libraryeventsproducer.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@Entity
 public class LibraryEvent {
 
+    @Id
+    @GeneratedValue
     private Integer libraryEventId;
+    @Enumerated(EnumType.STRING)
     private LibraryEventType libraryEventType;
+    @OneToOne(mappedBy = "libraryEvent", cascade = CascadeType.ALL)
+    @ToString.Exclude
     private Book book;
 }
